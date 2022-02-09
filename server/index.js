b5115c335dcafd5c0fd4dab9dd68e250501638bb
sarrazol <sebastianarrazola@gmail.com>
2022-02-09 01:46:20 -0500

    make it better

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Have Node serve the files for our built React app
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
